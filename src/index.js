9dc345151c53666b097c41ed21ce68752ade253d
AriMaireth <ari.maireth@gmail.com>
2020-03-03 08:56:30 -0500

    actualización tarjeta valida

--- src/index.js
+++ src/index.js
@@ -1,44 +1,59 @@
 import validator from './validator.js';
+
 const pagina1 = document.getElementById('primeraPagina');
 const pagina2 = document.getElementById('segundaPagina');
 const siguiente = document.getElementById('Siguiente');
 const validar = document.getElementById('validarT');
 const pagina3 = document.getElementById('terceraPagina');
 const regresar = document.getElementById('regresar');
+const usuario = document.getElementById('usuario');
+const pagina3h1 = document.getElementById('pagina3h1');
+const pagina3h2 = document.getElementById('pagina3h2');
+const tarjetaInvalida = document.getElementById('tarjetaInvalida');
 
-    pagina1.style.display = ' ';
-    pagina2.style.display = 'none';
-    pagina3.style.display = 'none';
+const numeroTarjetaV = document.getElementById('numeroTarjeta').value;
 
-siguiente.addEventListener('click', function siguiente1(){
-  if (usuario.value =="") {
-    alert ('no has introducido tu nombre de usuario')
+pagina1.style.display = ' ';
+pagina2.style.display = 'none';
+pagina3.style.display = 'none';
+
+siguiente.addEventListener('click', function siguiente1() {
+  if (usuario.value == "") {
+    alert('no has introducido tu nombre de usuario')
   }
   else {
-    pagina1.style.display ='none';
-    pagina2.style.display ='block';
-    pagina3.style.display ='none';
-  const usuario= document.getElementById('usuario').value;
-  const pagina2h1= document.getElementById('pagina2h1');
-pagina2h1.textContent ='HOLA ' + usuario.toUpperCase() + ' BIENVENIDO!'};
+    pagina1.style.display = 'none';
+    pagina2.style.display = 'block';
+    pagina3.style.display = 'none';
+    const usuario = document.getElementById('usuario').value;
+    const pagina2h1 = document.getElementById('pagina2h1');
+    pagina2h1.textContent = '¡HOLA ' + usuario.toUpperCase() + ' BIENVENIDO!';
+  }
 });
 
-   validar.addEventListener('click',function checkLength(numeroTarjeta){
-    const numeroTarjetaV= document.getElementById('numeroTarjeta').value;
-    if (numeroTarjetaV.length ===16) {
-      pagina1.style.display ='none';
-      pagina2.style.display ='none';
-      pagina3.style.display ='block';
-      let array = Array.from(numeroTarjetaV);
-      let array2 =[];
-      for ( let i=numeroTarjetaV.length-1;i>=0;i--){
-        array2.push(array[i]);
-        if (true) {
+validar.addEventListener('click', function checkLength() {
+  if (validator.isValid(numeroTarjetaV) == true) {
+    pagina1.style.display = 'none';
+    pagina2.style.display = 'none';
+    pagina3.style.display = 'block';
 
-        }
-        console.log(array2);
-      }
+    validator.maskify(numeroTarjetaV);
+    pagina3h1.textContent = validator.maskify(numeroTarjetaV);
+    pagina3h2.textContent = '¡Tu registro ha sido exitoso!';
+  }
+  else {
+    pagina1.style.display = 'none';
+    pagina2.style.display = 'none';
+    pagina3.style.display = 'block';
+    pagina3h1.textContent = validator.maskify(numeroTarjetaV);
+    pagina3h2.textContent = '¡oh oh! Tarjeta invalida';
+    tarjetaInvalida.textContent = 'Verifica el número de tu tarjeta e intentalo de nuevo.';
+  }
+});
 
-     }else {
-      alert('ingreso invalido')};
-  });
+regresar.addEventListener('click', function regresar() {
+  pagina1.style.display = 'none';
+  pagina2.style.display = 'block';
+  pagina3.style.display = 'none';
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
 const validator = {
+  isValid: (numeroTarjetaV) => {
+    let array1 = Array.from(numeroTarjetaV);
+    let arrayRev = [];
+    let sumaTotal = 0;
 
+    for (let i = array1.length - 1; i >= 0; i--) {
+      arrayRev.push(array1[i]);
+    }
+    for (let j = 0; j <= arrayRev.length - 1; j++) {
+      if (j % 2 == 0) {
+        sumaTotal += parseInt(arrayRev[j]);
+      }
+      else if (2 * arrayRev[j] < 10) {
+        sumaTotal += parseInt(arrayRev[j] * 2);
+      }
+      else {
+        let mult = (2 * arrayRev[j]).toString()
+        let mult1 = parseInt(mult.charAt(0))
+        let mult2 = parseInt(mult.charAt(1))
+        let sumaMult = mult1 + mult2;
+        sumaTotal += sumaMult
+      }
+    }
+
+    if (sumaTotal % 10 == 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+
+  }
+
+  ,
+  maskify: (numeroTarjetaV) => {
+    let trfNumero = numeroTarjetaV.slice(-4);
+    let maskiNumero = " ";
+    for (let i = 0; i < numeroTarjetaV.length - 4; i++) {
+      maskiNumero += '#';
+    }
+    return maskiNumero + trfNumero;
+  }
 
 };
 
